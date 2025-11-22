267547d27cf16d64e4ba76966fcb7cacdf7c47dc
tmc39 <tmc39@njit.edu>
2025-11-22 10:29:20 -0500

    Created framework for backend login

---
+++ Frontend/index.php
@@ -0,0 +1,19 @@
+<?php
+//Starts the php session
+  session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>EventLink</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php
+  include("EventPage.php");
+  ?>
+</head>
+<body>
+  <app-root></app-root>
+</body>
+</html>
--- nav.inc.php
+++
@@ -1,7 +0,0 @@
-<!--Searchbar for main page-->
-<form action="index.php" method="post">
-<label>Search for an event:</label><br>
-<input type="text" name="placeholder" size="14" />
-<input type="submit" value="find" />
-<input type="hidden" name="content" value="placeholder">
-</form>
